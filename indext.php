abb634f888c6d1fe500a8037915febd7a01d345a
rashel9454 <rashelrana931@gmail.com>
2022-03-25 15:51:58 +0600

    asinment2

--- indext.php
+++ indext.php
@@ -1,4 +1,10 @@
 <?php 
-$name ="rashel rana ";
-$name = 'farhana';
-echo $name ;
+	//	I have three old hp monitors, not new. I want to sell it on my own website.
+    // That’s why I should create a product upload form on my website, then upload my product with
+    //specification and available quantity. My monitors have a 15.6
+    // inch display. My monitor’s VGA cable status, yes, I will sell it for 8000 tk.
+
+$name = "hp monitor"; //datatype= string
+$selprize = 800; //datatype= integer
+$monitor_size= 15.6; // datatype=float,double
+$sell_it_on_my_own_website = true; //datatype =Boolean
